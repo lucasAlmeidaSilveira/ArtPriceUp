c41d84f11e6cf58ca99ff8808bb6b26b202d1554
Lucas Almeida da Silveira <lucas.asilveira.sh@gmail.com>
2023-02-27 11:50:59 -0300

    need correction bug

--- index.js
+++ index.js
@@ -19,16 +19,40 @@ async function click(btn, page) {
   await page.click(btn);
 }
 
-async function updateInputValue(page) {
-  const inputSize =
-    'div#ProdutoEstoqueCombinacaoAtributo2Id_chosen > a.chosen-single span';
-  const expectedText = '30cm x 45cm';
+async function updateValues(page, value) {
   const inputValue = 'input#ProdutoEstoqueValorVenda';
   const checkBoxPromo = '#ProdutoEstoquePromocao';
   const inputPercentual = '#ProdutoEstoquePercentualPromocao';
   const inputInitPromo = '#ProdutoEstoquePromocaoDataInicio';
   const inputEndPromo = '#ProdutoEstoquePromocaoDataFim';
   const btnSubmit = 'button.btn.btn-icon.btn-submit';
+
+  await page.$eval(inputValue, input => (input.value = value));
+
+  // Clica no botão de Promoção
+  await click(checkBoxPromo, page);
+
+  // Atualiza valor de desconto para 30
+  await page.$eval(inputPercentual, input => (input.value = '30'));
+
+  // Atualiza valor data de inicio da promoção
+  await page.$eval(inputInitPromo, input => (input.value = '23/02/2023'));
+
+  // Atualiza valor data de fim da promoção
+  await page.$eval(inputEndPromo, input => (input.value = '01/01/2024'));
+
+  // Clica no botão Salvar variação
+  await click(btnSubmit, page);
+}
+
+async function updateInputValue(page) {
+  const inputSize =
+    'div#ProdutoEstoqueCombinacaoAtributo2Id_chosen > a.chosen-single span';
+  const sizeP = '30cm x 45cm';
+  const sizeM = '40cm x 60cm';
+  const sizeG = '60cm x 90cm';
+
+  const valueP = '227,14';
 
   // Espera até que o seletor da tag <a> esteja disponível na página
   await page.waitForSelector(inputSize);
@@ -37,23 +61,8 @@ async function updateInputValue(page) {
   const spanText = await page.$eval(inputSize, span => span.textContent.trim());
 
   // Atualiza valores
-  if (spanText === expectedText) {
-    await page.$eval(inputValue, input => (input.value = '227,14'));
-
-    // Clica no botão de Promoção
-    await click(checkBoxPromo, page);
-
-    // Atualiza valor de desconto para 30
-    await page.$eval(inputPercentual, input => (input.value = '30'));
-
-    // Atualiza valor data de inicio da promoção
-    await page.$eval(inputInitPromo, input => (input.value = '23/02/2023'));
-
-    // Atualiza valor data de fim da promoção
-    await page.$eval(inputEndPromo, input => (input.value = '01/01/2024'));
-
-    // Clica no botão Salvar variação
-    click(btnSubmit, page);
+  if (spanText === sizeP) {
+    await updateValues(page, valueP);
   }
 }
 
