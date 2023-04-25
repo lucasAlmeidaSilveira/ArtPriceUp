9b5e48fffdea297d0cf630dec4acddd867891fad
Lucas Almeida da Silveira <lucas.asilveira.sh@gmail.com>
2023-04-25 08:21:25 -0300

    update in function updateTitle. Necessary adjusts

--- src/controllers/updateTitle.js
+++ src/controllers/updateTitle.js
@@ -1,22 +1,24 @@
-import { findAmountFrames, findCategorie, findValueInput } from "./tools.js"
+import { click, findAmountFrames, findCategorie, findValueInput } from "./tools.js"
 
 export async function updateTitle(page){
-	const categorie = await findCategorie(page)
+	let categorie = await findCategorie(page)
 	const oldTitle = await findValueInput(page, "#ProdutoNome")
 	const amountFrames = await findAmountFrames(page)
+	const btnSave = " btn-toolbar-fixed a.btn-submit-false" || "a.btn-submit-false"
 	// const pageProducts = "https://www.outletdosquadros.com.br/painel/catalogo/produtos/index"
-	// const btnSave = " btn-toolbar-fixed a.btn-submit-false" || "a.btn-submit-false"
 
 	if (!oldTitle.includes("Espelho")) {
 		if (!oldTitle.includes("Quadro Decorativo")) {
 			const formattedTitle = oldTitle
-				.replace("Kit", "")
-				.replace("quadro", "")
-				.replace("quadros", "")
+				.replace(/kit?/gi, "")
+				.replace(/quadro?/gi, "")
+				.replace(/quadros?/gi, "")
 				.replace(/\d+/g, "")
 				.trim()
   
-			const newTitle = `Quadro decorativo ${amountFrames} telas ${categorie} ${formattedTitle}`
+			categorie === "Todos os quadros" ? categorie = "" : categorie
+
+			const newTitle = `Quadro Decorativo ${amountFrames} Telas${categorie} ${formattedTitle}`
 			await page.$eval("#ProdutoNome", (el, value) => el.value = value, newTitle)      
 		}
 	}
